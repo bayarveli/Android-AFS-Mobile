51d9bbd6f7e1d9526f66a9c5c2512984b13dd86d
bayarveli <bayar.veli@gmail.com>
2021-03-06 11:27:52 +0300

    getting data from dynamically created fragments

--- app/src/main/java/com/example/afs_mobile/MyConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.afs_mobile;
-
-public abstract class MyConstants {
-    public static final String HOUR = "time_hour";
-    public static final String MINUTE = "time_minute";
-    public static final String TIME_PICKER = "time_picker";
-}
